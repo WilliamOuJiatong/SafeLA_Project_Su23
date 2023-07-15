2fc6e8ff874b4f83a901652b856f711808850f0a
jianyq <yuqingj2@illinois.edu>
2023-07-15 16:18:28 -0500

    update map

--- src/frontend/src/Home.js
+++ src/frontend/src/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function Home() {
+const Home = () => {
+  const [position, setPosition] = useState(null);
+
+  const MapEvents = () => {
+    const map = useMapEvents({
+      click: (e) => {
+        setPosition(e.latlng);
+      },
+    });
+    return null;
+  }
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <MapContainer center={[34.0522, -118.2437]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MapEvents />
+      </MapContainer>
+      {position && <p>Latitude: {position.lat}, Longitude: {position.lng}</p>}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
