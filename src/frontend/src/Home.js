776e4908443386424820f56294b2d78422932b92
jianyq <yuqingj2@illinois.edu>
2023-07-18 14:26:04 -0500

    update

--- src/frontend/src/Home.js
+++ src/frontend/src/Home.js
@@ -4,6 +4,7 @@ import 'leaflet/dist/leaflet.css';
 
 const Home = () => {
   const [position, setPosition] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const MapEvents = () => {
     const map = useMapEvents({
@@ -14,9 +15,22 @@ const Home = () => {
     return null;
   }
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <div>
-      <MapContainer center={[34.0522, -118.2437]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <div style={{ padding: '10px' }}>
+        <input
+          type="text"
+          placeholder="Search here..."
+          value={searchTerm}
+          onChange={handleChange}
+          style={{ width: '100%', padding: '10px' }}
+        />
+      </div>
+      <MapContainer center={[34.0522, -118.2437]} zoom={13} style={{ height: "70vh", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
