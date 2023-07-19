ef9258ce1df0db3a879d8a9dc0f8a3a302d8bd4d
Jiatong Ou <jiatongou@JiatongdeMacBook-Pro.local>
2023-07-18 21:24:52 -0500

    some format editing

--- src/frontend/src/Login.js
+++ src/frontend/src/Login.js
@@ -13,49 +13,47 @@ function Login() {
     const [errors, setErrors] = useState({})
     const handleInput = (event) => {
         console.log('$$$', event.target.name)
-        console.log('$$$',event.target.value)
-        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+        console.log('$$$', event.target.value)
+        setValues(prev => ({ ...prev, [event.target.name]: [event.target.value] }))
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        setErrors( Validation(values));
-        if(errors.email === "" && errors.password === "") {
+        setErrors(Validation(values));
+        if (errors.email === "" && errors.password === "") {
             axios.post('http://localhost:8081/login', values)
-            .then(res => {
-                if (res.data === "Success") {
-                    navigate('/home');
-                }
-                else {
-                    alert("No record existed");
-                }
-            })
-            .catch(err => console.log(err));
+                .then(res => {
+                    if (res.data === "Success") {
+                        navigate('/home');
+                    }
+                    else {
+                        alert("No record existed");
+                    }
+                })
+                .catch(err => console.log(err));
         }
     }
     return (
-        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>  
-            <div className='bg-white p-3 rounded w-25'>  
-                <h2>Sign-In</h2> 
+        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2>Sign-In</h2>
                 <form action="" onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder='Enter Email' name='email' 
-                        onChange={handleInput} className='form-control rounded-0'/>
+                        <input type="email" placeholder='Enter Email' name='email'
+                            onChange={handleInput} className='form-control rounded-0' />
                         <span className='text-danger'> {errors.email}</span>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
-                        <input type="password" placeholder='Enter Password' name='password' 
-                        onChange={handleInput} className='form-control rounded-0'/>
+                        <input type="password" placeholder='Enter Password' name='password'
+                            onChange={handleInput} className='form-control rounded-0' />
                         <span className='text-danger'> {errors.password}</span>
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'> Log in</button>
                     <p>You agree to our terms and policies</p>
                     <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
                 </form>
-
             </div>
-
         </div>
     )
 }
